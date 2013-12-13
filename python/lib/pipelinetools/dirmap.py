49467ec80ad66adb7db25fcdf791ad6c9b254f88
cwmartin <martin.christopherw@gmail.com>
2013-12-13 01:13:49 -0500

    comment and command-ified dirmap

--- python/lib/pipelinetools/dirmap.py
+++ python/lib/pipelinetools/dirmap.py
@@ -1,21 +1,32 @@
+"""
+
+"""
 import os
 import yaml
 import platform
 import logging
+import sys
 
 log = logging.getLogger(__name__)
 
 # DEFAULT_DIRMAP_FILE = "%s/src/config/dirmap.yml" % os.environ("TBE_TOOLS_ROOT")
 
+class DirmapError(Exception):pass
+
 DEFAULT_DIRMAP_FILE = r"C:\Users\Christopher\Google Drive\github\pipelinetools\python\lib\pipelinetools\dirmap.yml"
 
-def read_dirmap():
+def _read_dirmap():
     """
+    Load the dirmap from the dirmap file.
     """
     dirmap_file = os.environ.get("TBE_DIRMAP_FILE", False)
+    
     if not dirmap_file:
         dirmap_file = DEFAULT_DIRMAP_FILE
 
+    if not os.path.exists(dirmap_file):
+        raise DirmapError("Unablet to find dirmap file: '%s'" % dirmap_file)
+        
     with open(dirmap_file, "r") as dirmap_file:
         dirmaps_data = yaml.load(dirmap_file.read())
 
@@ -24,7 +35,12 @@ def read_dirmap():
 
 def dirmap(path, os_name=None):
     """
-
+    Find the mapping for a path. This will run through all directory mappings
+    looking for a path match. If found, returns the remapped path.
+    @param path The path to remap
+    @param os_name If specified, remap the path to the specified OS. If None, the 
+    current OS is used. Valid values are "linux" and "windows"
+    @return Remapped path string.
     """
     log.debug("Dirmapping path: '%s'" % path)
 
@@ -37,13 +53,33 @@ def dirmap(path, os_name=None):
     if os_name is None:
         os_name = curr_platform
 
-    dirmaps = read_dirmap()
+    dirmaps = _read_dirmap()
 
     for dirmap in dirmaps:
         for dirmap_platform in dirmap:
             if path.startswith(dirmap[dirmap_platform]):
                 repl = dirmap[os_name]
                 return path.replace(dirmap[dirmap_platform], repl)
-    raise Exception("Unable to find dirmap for path '%s'" % path)
 
-print dirmap("/mnt/T/mypath/blarg")
+    raise DirmapError("Unable to find dirmap for path '%s'" % path)
+
+if __name__ == "__main__":
+    import optparse
+
+    parser = optparse.OptionParser(usage="usage: dirmap [options] path")
+    parser.add_option("-o", "--os", action="store", dest="os_name", default=None,
+        help="OS to remap to. Valid values are 'linux' and 'windows'")
+
+    options, args = parser.parse_args()
+
+    if len(args) != 1:
+        parser.print_usage()
+        sys.exit(-1)
+
+    try:
+        path = dirmap(args[0], os_name=options.os_name)
+        print path
+        sys.exit(0)
+    except DirmapError, why:
+        print why
+        sys.exit(-2)
